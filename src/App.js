5e15121fabe4574321b7d0a6ffe7d578770f4614
Vincent Desforges <desforges.vincent@gmail.com>
2018-09-12 10:28:14 +0100

    added the files for the views that will be routed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 import Layout from './hoc/Layout/Layout';
+import Strategy from './components/Strategy/Strategy';
+import Strategies from './containers/Strategies/Strategies';
+import NewStrategy from './containers/NewStrategy/NewStrategy';
 
 class App extends Component {
   render() {
@@ -8,6 +11,9 @@ class App extends Component {
       <div>
         <Layout>
           <p>child into layout</p>
+          <Strategy />
+          <Strategies />
+          <NewStrategy />
         </Layout>
       </div>
     );
---
+++ src/containers/NewStrategy/NewStrategy.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './NewStrategy.css';
+
+class NewStrategy extends Component {
+  render() {
+    return (
+      <p className="NewStrategy">NewStrategy</p>
+    );
+  };
+}
+
+export default NewStrategy;
