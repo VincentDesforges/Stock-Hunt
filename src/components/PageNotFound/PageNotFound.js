351908b4505c7537bf54c308225e848abc3ccec3
Vincent Desforges <desforges.vincent@gmail.com>
2018-09-15 13:57:48 +0100

    end of day commit

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './PageNotFound.css'
+
+const pageNotFound = () => (
+  <div className="PageNotFound">
+    <h1>404: Page Not Found...</h1>
+  </div>
+);
+
+export default pageNotFound;
