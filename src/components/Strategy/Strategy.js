4822d43d5facd4a40530f9d0e34e391e393afbf9
Vincent Desforges <desforges.vincent@gmail.com>
2018-09-16 21:32:12 +0100

    added the detailed card in the show view

--- src/components/Strategy/Strategy.js
+++ src/components/Strategy/Strategy.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import DetailedCard from '../DetailedCard/DetailedCard';
 import './Strategy.css'
 
+const tempProps = {
+  ticker: 'AAPL',
+  exchange: 'NASDAQ',
+  price: '223.85',
+  change: '5.53',
+  percentage_change: '2.54',
+  volume: '448,385',
+  pe_ratio: '20.28',
+  eps: '11.04',
+  dividend_yield: '1.31%'
+};
+
 const strategy = (props) => (
-  <p className="Strategy">Strategy</p>
+  <div className="Strategy">
+    <DetailedCard stockData={tempProps}/>
+  </div>
 );
 
 export default strategy;
