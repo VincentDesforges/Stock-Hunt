f91f2a86496ee7ec051f62aa191d10837b1409b2
Vincent Desforges <desforges.vincent@gmail.com>
2018-09-17 07:54:03 +0100

    added state in strategies

--- src/containers/Strategies/Strategies.js
+++ src/containers/Strategies/Strategies.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 import './Strategies.css';
 
 class Strategies extends Component {
+  state = {
+    searchField: '',
+    orderBy: '', // most popular, most recent
+    filter: '', // strategy type (value, growth, short, etc...)
+  }
+
   render() {
     return (
       <p className="Strategies">Strategies</p>
